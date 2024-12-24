2e2c18cf9d6652c82cf90ff7de4b2e18d60cd1d2
Diya Shah <shah.diya1094@gmail.com>
2024-12-24 23:47:39 +0530

    Complete

---
+++ assets/includes/footer.php
@@ -0,0 +1,50 @@
+<footer class="section-p1">
+        <div class="col">
+            <img src="assets/image/dr logo 12-01.png" class="logo" alt="logo" width="100px" height="100px">
+            <h4 style="color: maroon;">Contact</h4>
+            <p style="color: black;"><strong>Address:</strong>Ahmedabad.</p>
+            <p style="color: black;"><strong>Phone:</strong>(+91) 6354010001</p>
+            <p style="color: black;"><strong>Hours:</strong>10:00 - 18:00, Mon-Sat</p>
+            <div class="follow">
+                <h4 style="color: maroon;">Follow us</h4>
+                <div class="icon">
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-pinterest-p"></i>
+                    <i class="fab fa-youtube"></i>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <h4 style="color: maroon;">About</h4>
+            <a href="aboutM.php">About us</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms & Conditions</a>
+            <a href="contactM.php">Contact us</a>
+        </div>
+
+        <div class="col">
+            <h4 style="color: maroon;">My Account</h4>
+            <a href="aboutM.php">About us</a>
+            <a href="cartM.php">View Cart</a>
+            <a href="#">Help</a>
+        </div>
+        <div class="col install">
+            <h4 style="color: maroon;">Trusted Payment Options</h4>
+            <p style="color: maroon;">From Government of India</p>
+            <div class="row">
+                <img src="assets/image/pay/mobile-banking.png" width="70px" height="50px" alt="">
+                <img src="assets/image/pay/Bhim-Upi-Logo.png" width="300px" height="50px" alt="">
+            </div>
+            <p style="color: maroon;">Secured payment Gateway</p>
+            <img src="assets/image/pay/razorpay.png" width="250px" height="60px">
+        </div>
+    </footer>
+    <div class="new-copy" style="display: flex; justify-content: center; align-items: center; text-align: center; background-color: #774936; padding: 0; height:50px">
+        <p style="font-size: 14px; color: #fff;">&copy; 2024 Decoriz. All rights reserved.</p>
+    <script src="js/script.js"></script>
+    <script src="js/redirect_product.js"></script>
+</body>
+</html>
---
+++ assets/includes/headerR.php
@@ -0,0 +1,57 @@
+<?php
+
+// start session
+session_start();
+
+// get username from session array
+$uname = $_SESSION['uname'];
+
+// variables for sql connection 
+include('assets/includes/dbconnection.php');
+
+// establish sql connection
+
+// select first name, last name of user 
+$sql = "SELECT Name FROM tbluser WHERE Email='$uname';";
+
+// run and save query
+$result = $con->query($sql);
+
+if($result->num_rows == 1)
+{
+    // update fname and lname if any changes were made
+    while($row = $result->fetch_assoc())
+    {
+        $_SESSION["fName"] = $row["Name"];
+    }
+}
+
+// close sql connection
+// $con->close();
+
+// retrieve first name and last name of user from session array
+$fname = $_SESSION['fName'];
+
+?>
+ 
+
+<img src="assets/image/dr logo 12-01.png" alt="DecoRiz Logo" width="100px" id="logo">
+        <nav id="main-nav">
+            <a href="homeR.php">Home</a>
+            <a href="cartR.php">Cart</a>
+            <a href="aboutR.php">About</a>
+            <a href="contactR.php">Contact</a>
+            <span class="right" style="width:120px;">
+												<div class="dropdown" style="width:100px;">
+													<span class="droplink" style="font-size:14px;"><?php echo "$fname"; ?></span>
+													<div class="dropdown-content">
+														<a href="proset2.php" style="font-size: medium;">Profile</a>
+                                                        <a href="orderh2.php" style="font-size: medium;">Order History</a>
+														<a href="logout.php" style="font-size: medium;">Logout</a>
+												</div>
+											</div>
+										</span>
+
+            <!-- <a href="signin.php">Sign-In</a> -->
+
+        </nav>
